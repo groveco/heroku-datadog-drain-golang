1985c11777d2d44a12bac57aeb361926ea449df5
Pierre Negri <34521951+pierrenegri@users.noreply.github.com>
2018-09-06 15:26:15 -0700

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', seconds=5)
+@sched.scheduled_job('interval', seconds=1)
 def timed_job():
   os.system('heroku logs -n 1500 --app vast-cove-11097  >> heroku-dd.txt')
 
