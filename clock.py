1a797f550f48db833f3840771bf0bb45884bf8f3
Pierre Negri <34521951+pierrenegri@users.noreply.github.com>
2018-09-06 14:42:55 -0700

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,12 +2,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', seconds=5)
 def timed_job():
-    print('This job is run every three minutes.')
+    import os
+os.system('heroku logs -n 1500 --app vast-cove-11097  >> heroku-dd.txt')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
 
 sched.start()
