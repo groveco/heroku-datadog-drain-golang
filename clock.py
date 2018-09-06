a26e590c614f4f5208d80846a64dae22ec79e4f1
Pierre Negri <34521951+pierrenegri@users.noreply.github.com>
2018-09-06 15:19:52 -0700

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=5)
 def timed_job():
-os.system('heroku logs -n 1500 --app vast-cove-11097  >> heroku-dd.txt')
+  os.system('heroku logs -n 1500 --app vast-cove-11097  >> heroku-dd.txt')
 
 
 sched.start()
